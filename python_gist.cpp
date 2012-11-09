74097650e5ee461e28b3fc41d4d6ddb57f345635
Mike Mills <mike@maclaptop.(none)>
2012-11-08 21:13:36 -0700

    GPU version has a nan problem

--- python_gist.cpp
+++ python_gist.cpp
@@ -1,6 +1,7 @@
 
 #include "python_gist.h"
 #include <vector>
+#include <cmath>
 #include <opencv/cv.h>
 #include <opencv2/gpu/gpu.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -10,12 +11,14 @@
 
 #include <numpy/ndarrayobject.h>
 
+#define KERNEL_SIZE 21
 
 int IMAGE_HEIGHT = 240, IMAGE_WIDTH = 320;
 //std::vector<cv::Mat>       *Gabor_filters;
 std::vector<cv::gpu::GpuMat> *Gabor_filters;
 std::vector<cv::Mat>         *Response_Image;
 std::vector<cv::Mat>         *Response_Image_test;
+
 
 /*
 inline cv::Mat Get_cvMat_From_Numpy_Mat(cv::Mat matin) 
@@ -48,16 +51,16 @@ inline void Get_cvMat_From_Numpy_Mat(PyArrayObject *matin, cv::Mat &output, int 
 }
 //=============================================================================================================================
 
-cv::Mat mkGaborKernel(int ks, float sig, float th, float lm, float ps)
+cv::Mat mkGaborKernel(int ks, double sig, double th, double lm, double ps)
 {
     int hks = (ks-1)/2;
-    float theta = th*CV_PI/180;
-    float psi = ps*CV_PI/180;
-    float del = 2.0/(ks-1);
-    float lmbd =  0.5+lm/100.0;
-    float sigma = sig/ks;
-    float x_theta;
-    float y_theta;
+    double theta = th*CV_PI/180;
+    double psi = ps*CV_PI/180;
+    double del = 2.0/(ks-1);
+    double lmbd =  0.5+lm/100.0;
+    double sigma = sig/ks;
+    double x_theta;
+    double y_theta;
     cv::Mat kernel(ks,ks, CV_32F);
     for (int y=-hks; y<=hks; y++)
     {
@@ -65,7 +68,7 @@ cv::Mat mkGaborKernel(int ks, float sig, float th, float lm, float ps)
         {
             x_theta = x*del*cos(theta)+y*del*sin(theta);
             y_theta = -x*del*sin(theta)+y*del*cos(theta);
-            kernel.at<float>(hks+y,hks+x) = (float)exp(-0.5*(pow(x_theta,2)+pow(y_theta,2))/pow(sigma,2))* cos(2*CV_PI*x_theta/lmbd + psi);
+            kernel.at<double>(hks+y,hks+x) = (double)exp(-0.5*(pow(x_theta,2)+pow(y_theta,2))/pow(sigma,2))* cos(2*CV_PI*x_theta/lmbd + psi);
         }
     }
     return kernel;
@@ -75,19 +78,16 @@ std::vector<cv::gpu::GpuMat> *create_gabor(int nscales,  int *orientations)
 
 {
     int nfilters = 0;
-    int kernel_size = 21;
-    
+        
     for (int i=0; i < nscales; i++) nfilters += orientations[i];
 
-    //std::vector<cv::Mat> *Gfs          = new std::vector<cv::Mat>(nfilters);
     std::vector<cv::gpu::GpuMat> *Gfs = new std::vector< cv::gpu::GpuMat >(nfilters);
 
     int filter = 0;
     for (int scale = 0; scale < nscales; scale++){
         for (int ori =0; ori < orientations[scale]; ori++) {
-            printf("Allocate gabor gpu buffer %d\n", filter);
-            (*Gfs)[filter] = cv::gpu::GpuMat(kernel_size, kernel_size, CV_32F);
-            (*Gfs)[filter].upload(mkGaborKernel(kernel_size, 4 - scale, ori * 90/orientations[scale], 50, 90));
+            (*Gfs)[filter] = cv::gpu::GpuMat(KERNEL_SIZE, KERNEL_SIZE, CV_64F);
+            (*Gfs)[filter].upload(mkGaborKernel(KERNEL_SIZE, 4 - scale, ori * 90/orientations[scale], 50, 90));
             filter++;
         }
     }
@@ -104,7 +104,7 @@ std::vector<cv::Mat> *response_init(int width, int height)
     
 	for (int scl=0; scl < N_SCALES; scl++) {
 		for (int ori=0; ori < orientations[scl]; ori++) {
-			response_images->push_back(cv::Mat(height+1, width+1, CV_32FC1)); // +1 for some Integral image stuff later
+			response_images->push_back(cv::Mat(height+1, width+1, CV_64F)); // +1 for some Integral image stuff later
             
 		}
 	}
@@ -114,7 +114,7 @@ std::vector<cv::Mat> *response_init(int width, int height)
 //=============================================================================================================================
 
 
-cv::Mat tmp_image(IMAGE_HEIGHT, IMAGE_WIDTH, CV_32FC1);
+cv::Mat tmp_image; //(IMAGE_HEIGHT, IMAGE_WIDTH, CV_64FC1);
 
 void format_image(cv::Mat &input, cv::Mat &output)
 {
@@ -122,8 +122,7 @@ void format_image(cv::Mat &input, cv::Mat &output)
     cv::resize(input, output, cv::Size(IMAGE_WIDTH,IMAGE_HEIGHT));
     
     cv::cvtColor(output, tmp_image, CV_BGR2GRAY);
-    tmp_image.convertTo(output, CV_32FC1, 1.0/255.0);
-    
+    tmp_image.convertTo(output, CV_64F, 1.0/255.0);
     
 }
 //=============================================================================================================================
@@ -138,8 +137,8 @@ void format_image(cv::Mat &input, cv::Mat &output)
     for(j = 0; j < height; j++)
     {
         for(i = 0; i < width; i++) {
-            //((float*)im.data)[j*im.cols+i] = (float)log( ((float*)im.data)[j*im.cols+i]+1.0f );
-            im.at<float>(j,i) = log(im.at<float>(j,i)+1.0);
+            //((double*)im.data)[j*im.cols+i] = (double)log( ((double*)im.data)[j*im.cols+i]+1.0f );
+            im.at<double>(j,i) = log(im.at<double>(j,i)+1.0);
         }
     }
     
@@ -153,17 +152,17 @@ void format_image(cv::Mat &input, cv::Mat &output)
 
     //
     // Build whitening filter and apply whitening filter
-    float s1 = fc/sqrt(log(2.0));
+    double s1 = fc/sqrt(log(2.0));
     for(j = 0; j < height; j++)
     {
         for(i = 0; i < width; i++)
         {
-            in1[j*width + i][0] = pim.at<float>(j,i);
+            in1[j*width + i][0] = pim.at<double>(j,i);
             in1[j*width + i][1] = 0.0f;
             
 
-            fx[j*width + i] = (float) i - width/2.0;
-            fy[j*width + i] = (float) j - height/2.0;
+            fx[j*width + i] = (double) i - width/2.0;
+            fy[j*width + i] = (double) j - height/2.0;
 
             gfc[j*width + i] = exp(-(fx[j*width + i]*fx[j*width + i] + fy[j*width + i]*fy[j*width + i]) / (s1*s1));
             
@@ -194,10 +193,10 @@ void format_image(cv::Mat &input, cv::Mat &output)
     {
         for(i = 0; i < width; i++)
         {
-            //((float *)pim.data)[j*pim.cols+i] -= in2[j*width+i][0] / (width*height);
-            pim.at<float>(j,i) -= in2[j*width+i][0] / (width*height);
+            //((double *)pim.data)[j*pim.cols+i] -= in2[j*width+i][0] / (width*height);
+            pim.at<double>(j,i) -= in2[j*width+i][0] / (width*height);
 
-            in1[j*width + i][0] = pim.at<float>(j,i) * pim.at<float>(j,i);
+            in1[j*width + i][0] = pim.at<double>(j,i) * pim.at<double>(j,i);
             in1[j*width + i][1] = 0.0;
 
             //printf("local %f\n", in1[j*width + i][0]);
@@ -224,7 +223,7 @@ void format_image(cv::Mat &input, cv::Mat &output)
     for(j = 0; j < height; j++)
     {
         for(i = 0; i < width; i++) {
-            pim.at<float>(j,i) = pim.at<float>(j,i) / (0.2+sqrt(sqrt(in2[j*width+i][0]*in2[j*width+i][0]+in2[j*width+i][1]*in2[j*width+i][1]) / (width*height)));
+            pim.at<double>(j,i) = pim.at<double>(j,i) / (0.2+sqrt(sqrt(in2[j*width+i][0]*in2[j*width+i][0]+in2[j*width+i][1]*in2[j*width+i][1]) / (width*height)));
         }
     }
     
@@ -236,44 +235,59 @@ void format_image(cv::Mat &input, cv::Mat &output)
     return res;
 }*/
     
+//    cv::gpu::GpuMat gpu_tmp(IMAGE_HEIGHT -22, IMAGE_WIDTH-22, CV_64F); // Might want to move this out to avoid any allocations that occur each time the function is called
+//    cv::Mat proctmp(IMAGE_HEIGHT -22, IMAGE_WIDTH-22, CV_64F);
+//    cv::gpu::GpuMat gpu_img(IMAGE_HEIGHT, IMAGE_WIDTH, CV_64F);
+
+cv::gpu::GpuMat gpu_img, gpu_tmp;
+cv::Mat         proc_img;
+
 //=============================================================================================================================
 void Process(cv::Mat &im)
 {
-    cv::gpu::GpuMat gpu_tmp(IMAGE_HEIGHT, IMAGE_WIDTH, CV_32F); // Might want to move this out to avoid any allocations that occur each time the function is called
-    
-    cv::gpu::GpuMat gpu_img(IMAGE_HEIGHT, IMAGE_WIDTH, CV_32F);
     
     //im = prefilt_process(im, 4);
     clock_t start = clock();
 
-    //cv::gpu::GpuMat gpu_int_img;
-
+    im.convertTo(im, CV_32F);
+    
     gpu_img.upload(im);
 
     for (unsigned int k=0; k < Gabor_filters->size(); k++) {
     
-        //cv::gpu::filter2D(gpu_img, gpu_tmp, CV_32F, (*Gabor_filters)[k]);
-        //printf("Convolve\n");
-        cv::gpu::convolve(gpu_img, (*Gabor_filters)[k], gpu_tmp);
-        //printf("power\n");
-        cv::gpu::pow(gpu_tmp, 2.0, gpu_tmp);
+        //cv::gpu::convolve(gpu_img, (*Gabor_filters)[k], gpu_tmp);
+        //cv::gpu::pow(gpu_tmp, 2.0, gpu_tmp);
         
-    
-        gpu_tmp.download(tmp_image);
+        //gpu_tmp.download(proc_img);
         
+        gpu_tmp.download(im);
 
-        if (k == 7) {
-            cv::normalize(tmp_image, tmp_image, 0, 1, CV_MINMAX);
-            cv::imshow("Training Images", tmp_image);
+        //cv::normalize(proc_img, proc_img, 0, 1, CV_MINMAX);
+        /*
+        if (k == 0) {
+            //cv::normalize(proc_img, proc_img, 0, 1, CV_MINMAX);
+            cv::imshow("Training Images", proc_img);
             //cv::waitKey(0);
         }
-    
-        cv::integral(tmp_image, (*Response_Image)[k]);
-    
+            
+        for (int y=0; y < proc_img.rows; y++)
+            for (int x = 0; x < proc_img.cols; x++)
+                if (std::isnan(proc_img.at<double>(y, x))) printf("*******PROC IMAGE FAIL*******\n"); 
+        */
+        
+        cv::Mat tmp;
+        //proc_img.convertTo(proc_im, CV_64F);
+        im.convertTo(im, CV_64F);
+        cv::integral(im, tmp);
+
+        for (int y=0; y < tmp.rows; y++)
+            for (int x = 0; x < tmp.cols; x++)
+                if (std::isnan(tmp.at<double>(y, x))) printf("&&&&&&&&&INTEGRAL FAIL&&&&&&&&&\n"); 
+
     }
 
     clock_t end = clock();
-    printf("Proc time %f\n", float(end - start)/CLOCKS_PER_SEC );
+    printf("Proc time %f\n", double(end - start)/CLOCKS_PER_SEC );
     
 }
 //=============================================================================================================================
@@ -283,7 +297,7 @@ void Process(cv::Mat &im)
 int nx[MAX_BLOCKS], ny[MAX_BLOCKS];
 
 
-void Fill_Descriptor(float *desc, 
+void Fill_Descriptor(double *desc, 
                      int xoffset,   int yoffset, 
                      int win_width, int win_height,
                      int xblks,     int yblks)
@@ -291,7 +305,7 @@ void Fill_Descriptor(float *desc,
 
     int i, x, y;
     
-    int cols = IMAGE_WIDTH, rows = IMAGE_HEIGHT;
+    int cols = IMAGE_WIDTH - (KERNEL_SIZE + 1), rows = IMAGE_HEIGHT- (KERNEL_SIZE + 1);
 
     int hlfimg_width   = cols/2;
     int hlfimg_height  = rows/2;
@@ -328,23 +342,26 @@ void Fill_Descriptor(float *desc,
     }
     
 
-    float denom = (float)(ny[1]-ny[0])*(nx[1]-nx[0]);
+    double denom = (double)(ny[1]-ny[0])*(nx[1]-nx[0]);
    
 
     for (unsigned int gbr = 0; gbr < Response_Image->size(); gbr++) {
 
         cv::Mat src = (*Response_Image)[gbr];
-        float *res = desc+gbr*xblks*yblks;
+        
+        double *res = desc+gbr*xblks*yblks;
 
         for(y = 0; y < yblks; y++) {
-            for(x = 0; x < xblks; x++) {  
-                float mean   = (  src.at<float>(ny[y+1], nx[x+1]) 
-                                 + src.at<float>(ny[y], nx[x]) 
-                                 - src.at<float>(ny[y+1], nx[x])
-                                 - src.at<float>(ny[y], nx[x+1]) );
+            for(x = 0; x < xblks; x++) { 
+
+                double mean   = (   src.at<double>(ny[y+1], nx[x+1]) 
+                                 + src.at<double>(ny[y], nx[x]) 
+                                 - src.at<double>(ny[y+1], nx[x])
+                                 - src.at<double>(ny[y], nx[x+1]) );
                 
 
                 res[y*xblks+x]=  mean / denom;
+                //if (std::isnan(res[y*xblks+x])) printf("blah %f %f %f %f %f %f\n", mean, denom, src.at<double>(ny[y+1], nx[x+1]), src.at<double>(ny[y], nx[x]), src.at<double>(ny[y+1], nx[x]), src.at<double>(ny[y], nx[x+1]) );
              }
         }
     
@@ -367,8 +384,8 @@ PyObject *PCA_project(PyObject *obj, PyObject *args)
         return NULL;
     }
 
-    Get_cvMat_From_Numpy_Mat(desc, cvdesc, CV_32FC1);
-    Get_cvMat_From_Numpy_Mat(pca_desc, cvpca_desc, CV_32FC1);
+    Get_cvMat_From_Numpy_Mat(desc, cvdesc, CV_64FC1);
+    Get_cvMat_From_Numpy_Mat(pca_desc, cvpca_desc, CV_64FC1);
 
     /*printf("%d %d : %d %d data %d %d : %d %d\n", cvdesc.cols, cvdesc.rows, cvpca_desc.cols, cvpca_desc.rows,   
                                                  pca_object.mean.cols, pca_object.mean.rows, pca_object.eigenstd::vectors.cols, pca_object.eigenstd::vectors.rows);
@@ -395,10 +412,18 @@ PyObject *Init_GIST(PyObject* obj, PyObject *args)
 	
 
 	Gabor_filters = create_gabor(N_SCALES, orientations);
-	Response_Image = response_init(cols, rows);
+	
 
 	IMAGE_WIDTH  = cols;
 	IMAGE_HEIGHT = rows;
+
+    tmp_image = cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_32F);
+    proc_img  = cv::Mat(IMAGE_HEIGHT-(KERNEL_SIZE+1), IMAGE_WIDTH-(KERNEL_SIZE+1), CV_32F);
+    gpu_img   = cv::gpu::GpuMat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_32F);
+    gpu_tmp   = cv::gpu::GpuMat(IMAGE_HEIGHT-(KERNEL_SIZE+1), IMAGE_WIDTH-(KERNEL_SIZE+1), CV_32F);
+    
+    Response_Image = response_init(IMAGE_HEIGHT-(KERNEL_SIZE+1), IMAGE_WIDTH-(KERNEL_SIZE+1));
+
 
 	Py_INCREF(Py_None);
 	return Py_None;
@@ -423,8 +448,8 @@ PyObject *Init_PCA(PyObject* obj, PyObject *args)
         return NULL;
     }
     
-    Get_cvMat_From_Numpy_Mat(mean, pca_object.mean, CV_32FC1);
-    Get_cvMat_From_Numpy_Mat(eigenstd::vectors, pca_object.eigenstd::vectors, CV_32FC1);
+    Get_cvMat_From_Numpy_Mat(mean, pca_object.mean, CV_64FC1);
+    Get_cvMat_From_Numpy_Mat(eigenstd::vectors, pca_object.eigenstd::vectors, CV_64FC1);
     
     PCcount = pca_object.eigenstd::vectors.rows;
 
@@ -482,7 +507,7 @@ PyObject *Descriptor_Allocate(PyObject *obj, PyObject *args)
     dims[0] = 1;
     dims[1] = DESCRIPTOR_SIZE(xblks, yblks);
 
-    if ( !(desc = (PyArrayObject*)PyArray_FromDims(2, dims, NPY_FLOAT)) ) {
+    if ( !(desc = (PyArrayObject*)PyArray_FromDims(2, dims, NPY_DOUBLE)) ) {
         printf("Error allocating Array\n");
         Py_INCREF(Py_None);
         return Py_None;
@@ -503,7 +528,7 @@ PyObject *Get_Descriptor(PyObject *obj, PyObject *args)
         return NULL;
     }
 
-    Fill_Descriptor((float *)(desc->data), xoffset, yoffset, win_width, win_height, xblks, yblks);
+    Fill_Descriptor((double *)(desc->data), xoffset, yoffset, win_width, win_height, xblks, yblks);
 
     Py_INCREF(Py_None);
     return Py_None;
